b21dc5f6bac845c21598d06f3b2b3ce89c2d6152
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-03-10 08:51:03 +0100

    the app is done, ready for deployment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { ChatEngine } from 'react-chat-engine';
 
+import LoginForm from './components/LoginForm';
 import ChatFeed from './components/ChatFeed';
 
 import './App.css';
 
 const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
   return (
     <ChatEngine
       height="100vh"
-      userName="Kiko"
-      userSecret="123123"
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
       projectID="164cb99a-65b0-4dd0-9a00-2f5cf0f7a0e9"
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
